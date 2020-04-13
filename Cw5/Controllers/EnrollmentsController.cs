b0b3c4405553e9268f8d39da16cd37194582221d
s18923 <s18923@pjwstk.edu.pl>
2020-04-13 10:49:51 +0200

    commit1

---
+++ Cw5/Controllers/EnrollmentsController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw5.DTOs;
+using Cw5.DTOs.Requests;
+using Cw5.DTOs.Responses;
+using Cw5.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cw5.Controllers
+{
+
+    [Route("api/enrollments")]
+    [ApiController]
+    public class EnrollmentsController : ControllerBase
+    {
+
+        private IStudentDbService service;
+     
+        public EnrollmentsController(IStudentDbService service)
+        {
+            this.service = service;
+        }
+
+        [HttpPost]
+        public IActionResult EntrollStudent(EnrollStudentRequest request)
+        {
+            var result = service.EnrollStudent(request);
+
+            switch (result.ResultCode)
+            {
+                case ResultCodes.NieWpisanoWszystkichDanychStudenta:
+                    return BadRequest("Nie wpisano poprawnie wszystkich danych studenta");
+                    
+                case ResultCodes.NieIstniejaStudia:
+                    return BadRequest("Studia nie istnieją");       
+                    
+                case ResultCodes.StudentJestJuzZapisanyNaSemest:
+                    return BadRequest("Student już jest zapisany na semestr 1!");
+                   
+                case ResultCodes.StudentJuzIstnieje:
+                    return BadRequest("Student już istnieje");                    
+            }
+
+            return Created("", result.Response);
+
+
+        }
+
+        [HttpPost("promotions")]
+        public IActionResult PromoteStudent(PromoteStudentRequest request)
+        { 
+
+
+            var result = service.PromoteStudents(request);
+
+            return Created("",result);
+        }
+    }
+}
---
+++ Cw5/Startup.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Cw5.Middlewares;
+using Cw5.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Cw5
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<IStudentDbService, SqlServerStudentDbService>();
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //------------------//
+
+            app.UseMiddleware<LoggingMiddleware>();
+
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.Headers.ContainsKey("Index"))
+                {
+                    context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                    await context.Response.WriteAsync("Nie poda³eœ indeksu");
+                    return;
+                }
+
+                string indexNumber = context.Request.Headers["Index"].ToString();
+
+                using (var con = new SqlConnection("Data Source=db-mssql;Initial Catalog=s18923;Integrated Security=True"))
+                using (var com = new SqlCommand())
+                {
+
+                    com.Connection = con;
+                    con.Open();
+
+                    com.CommandText = "select IndexNumber from Student where IndexNumber = @IndexNumber";
+                    com.Parameters.AddWithValue("IndexNumber", indexNumber);
+
+                    var dr = com.ExecuteReader();
+
+                    if (!dr.Read())
+                    {
+
+                        dr.Close();
+                        context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                        await context.Response.WriteAsync("Taki student nie istnieje");
+                        return;
+                    }                    
+
+                    dr.Close();
+                    com.Parameters.Clear();
+
+                    await next();
+                }
+            });
+            //-------------//
+
+           
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
